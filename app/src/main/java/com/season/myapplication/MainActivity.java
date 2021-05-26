215698f486023eeb374fe5bcc0e180b927f53788
451360508@qq.com <451360508@qq.com>
2021-05-26 11:59:21 +0800

    封装

--- app/src/main/java/com/season/myapplication/MainActivity.java
+++ app/src/main/java/com/season/myapplication/MainActivity.java
@@ -1,12 +1,13 @@
 package com.season.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.season.ecc.EccTool;
+
 import org.bitcoinj.core.ECKey;
-import org.bouncycastle.util.encoders.Base64;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,34 +17,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-            String data = "688109";
-            ECKey ecKey = new ECKey();
-            ecKey.decompress();
+        String data = "688109";
+        ECKey ecKey = EccTool.createKeypair();
 
-            Log.i("TAG", "onCreate: 签名内容：" + data);
-            Log.i("TAG", "onCreate: 私钥：" + ecKey.getPrivateKeyAsHex());
-            Log.i("TAG", "onCreate: 公钥：" + ecKey.getPublicKeyAsHex());
+        Log.i("TAG", "签名内容：" + data);
+        Log.i("TAG", "私钥：" + ecKey.getPrivateKeyAsHex());
+        Log.i("TAG", "公钥：" + ecKey.getPublicKeyAsHex());
 
-            String sign_msg1 = ecKey.signMessage(data);
-            Log.i("TAG", "onCreate: 签名：" + sign_msg1);
-
-            Log.i("TAG", "onCreate: 签名Base64解码：" + toHex(Base64.decode(sign_msg1)));
+        Log.i("TAG", "签名Base64解码：" + EccTool.sign(ecKey, data));
 
     }
 
 
-    public static String toHex(byte[] bytes) {
-        if (bytes == null) {
-            return "";
-        }
-        final char[] hexArray = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-        char[] hexChars = new char[bytes.length * 2];
-        int v;
-        for (int j = 0; j < bytes.length; j++) {
-            v = bytes[j] & 0xFF;
-            hexChars[j * 2] = hexArray[v >>> 4];
-            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-        }
-        return new String(hexChars);
-    }
 }
---
+++ lib_ecc/src/main/java/com/season/ecc/EccTool.java
@@ -0,0 +1,43 @@
+package com.season.ecc;
+
+import org.bitcoinj.core.ECKey;
+import org.bouncycastle.util.encoders.Base64;
+
+public class EccTool {
+
+    /**
+     * 创建密钥对
+     * @return
+     */
+    public static ECKey createKeypair() {
+        ECKey ecKey = new ECKey();
+        ecKey.decompress();
+        return ecKey;
+    }
+
+    /**
+     * 使用密钥对签名信息
+     * @param ecKey
+     * @param content
+     * @return
+     */
+    public static String sign(ECKey ecKey, String content) {
+        String sign_msg1 = ecKey.signMessage(content);
+        return toHex(Base64.decode(sign_msg1));
+    }
+
+    private static String toHex(byte[] bytes) {
+        if (bytes == null) {
+            return "";
+        }
+        final char[] hexArray = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        char[] hexChars = new char[bytes.length * 2];
+        int v;
+        for (int j = 0; j < bytes.length; j++) {
+            v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+}
